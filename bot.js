e8d161b40095c7d0c5b8b3611c07c8fcec7bc80b
0thaer0 <44973413+0thaer0@users.noreply.github.com>
2018-11-13 21:10:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "511963736186028054"; // ايدي السررفر
+var channel = "511963736186028058";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
